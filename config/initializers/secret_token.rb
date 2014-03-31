03cd57acc55ae86409fdcfe10164577bec4b6416
Sara Howden <lhsimpson@madisoncollege.edu>
2014-03-31 18:39:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExamGraderApp::Application.config.secret_key_base = '3559afc1fd79b79b45b32f9133e33dcfe63d8e0ad52af17c8c210181217e3681a78894daa3ae850a701e85db8a0747b973d1786e582b2da891383e46ef290630'
